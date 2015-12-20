a73aabf7eddbb0e80a9f53985785714c7c90e840
Ted Yeh <yeh.ted@gmail.com>
2015-12-20 16:43:07 -0600

    Not working yet

---
+++ topcoder/BearPaints.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class BearPaints {
+    public:
+    long maxArea(int W, int H, long M) {
+        long biggest = 0;
+        for(int i = W; i >= 0; i--)
+        {
+            for(int j = H; j >= 0; j--)
+            {
+                long big = long(i) * j;
+                if (big <= M)
+                {
+                    if (big > biggest) biggest = big;
+                    else return biggest;
+                }
+            }
+        }
+        return biggest;
+    }
+};
